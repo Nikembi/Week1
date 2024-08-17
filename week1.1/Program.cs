1c6d2b35f03ab57463cf3c58f15dbbc021cab567
Nikembi <119628940+Nikembi@users.noreply.github.com>
2024-08-17 15:23:12 -0500

    Add project files.

---
+++ week1.1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace week1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string aboutme = ("Hello, My name is Nikembi, An aspiring Full Stack Developer, " +
+                        "I'm excited to learn complex programming concepts, and eventually land my first job.");
+            Console.WriteLine(aboutme);
+
+            int product = Multiply(1, 2);
+            Console.ReadLine();
+            Console.WriteLine($"The product is: {product}");
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"This for loop has run {i} number of times");
+            }
+
+            string[] friends = {"Lisha", "Davion", "Colburn", "Kobe"};
+            foreach (string friend in friends)
+            {   
+                Console.WriteLine($"First friend is {friends[0]}");
+                Console.WriteLine($"First friend is {friends[1]}");
+                Console.WriteLine($"First friend is {friends[2]}");
+                Console.WriteLine($"First friend is {friends[3]}");
+                break;
+            }
+        }
+        //static string ForLoop()
+        //{
+        //    for (int i = 1; i <= 10; i++)
+        //    {
+        //        Console.WriteLine($"This for loop has run {i} number of times");
+
+        //        break;
+        //    }
+
+        //}
+        static int Multiply(int x, int y)
+            {
+
+                return x * y;
+            }
+    }
+}
