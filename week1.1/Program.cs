4632bf81781ac7960cbefd1ac7dc3d85b0fc6d5d
Nikembi Travis <nikembitravis@outlook.com>
2024-08-24 11:43:22 -0500

    Refactored the original for best practice

--- week1.1/Program.cs
+++ week1.1/Program.cs
@@ -2,47 +2,50 @@
 
 namespace week1._1
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            string aboutme = ("Hello, My name is Nikembi, An aspiring Full Stack Developer, " +
-                        "I'm excited to learn complex programming concepts, and eventually land my first job.");
-            Console.WriteLine(aboutme);
+            PrintAboutMe();
+            DemonstrateMultiplication();
+            RunForLoop();
+            PrintFriends();
+        }
 
+
+        static void PrintAboutMe()
+        {
+            string aboutMe = "Hello, My name is Nikembi, An aspiring Full Stack Developer. " +
+                             "I'm excited to learn complex programming concepts, and eventually land my first job.";
+            Console.WriteLine(aboutMe);
+        }
+
+        static void DemonstrateMultiplication()
+        {
             int product = Multiply(1, 2);
-            Console.ReadLine();
             Console.WriteLine($"The product is: {product}");
+        }
 
+        static void RunForLoop()
+        {
             for (int i = 0; i <= 10; i++)
             {
                 Console.WriteLine($"This for loop has run {i} number of times");
             }
+        }
 
-            string[] friends = {"Lisha", "Davion", "Colburn", "Kobe"};
-            foreach (string friend in friends)
-            {   
-                Console.WriteLine($"First friend is {friends[0]}");
-                Console.WriteLine($"Second friend is {friends[1]}");
-                Console.WriteLine($"Third friend is {friends[2]}");
-                Console.WriteLine($"Fourth friend is {friends[3]}");
-                break;
+        static void PrintFriends()
+        {
+            string[] friends = { "Lisha", "Davion", "Colburn", "Kobe" };
+            for (int i = 0; i < friends.Length; i++)
+            {
+                Console.WriteLine($"Friend {i + 1} is {friends[i]}");
             }
         }
-        //static string ForLoop()
-        //{
-        //    for (int i = 1; i <= 10; i++)
-        //    {
-        //        Console.WriteLine($"This for loop has run {i} number of times");
 
-        //        break;
-        //    }
-
-        //}
         static int Multiply(int x, int y)
-            {
-
-                return x * y;
-            }
+        {
+            return x * y;
+        }
     }
 }
